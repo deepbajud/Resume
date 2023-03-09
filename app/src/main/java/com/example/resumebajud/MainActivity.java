00a7f943d0b1678632f152a1055f4ab7e994b5c8
deepbajud <dbajud@gmail.com>
2023-03-09 09:54:31 +0530

    commit 5

--- app/src/main/java/com/example/resumebajud/MainActivity.java
+++ app/src/main/java/com/example/resumebajud/MainActivity.java
@@ -2,8 +2,7 @@ package com.example.resumebajud;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
+ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
@@ -20,10 +19,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button blinkingButton = findViewById(R.id.blinkingButton);
-        Animation blinkingAnimation = AnimationUtils.loadAnimation(this, R.anim.blinking_button_animation);
+        Animation myFadeInAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+        Animation myFadeOutAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out);
 
 
-        blinkingButton.startAnimation(blinkingAnimation);
+        blinkingButton.startAnimation(myFadeInAnimation);
+        blinkingButton.startAnimation(myFadeOutAnimation);
         blinkingButton.setOnClickListener(v -> {
 
 
--- app/src/main/java/com/example/resumebajud/Personal_Information.java
+++ app/src/main/java/com/example/resumebajud/Personal_Information.java
@@ -31,14 +31,27 @@ public class Personal_Information extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String pd01 = pd1.getText().toString();
-                String pd02  = pd1.getText().toString();
-                String pd03 = pd1.getText().toString();
-                String pd04 = pd1.getText().toString();
+                String pd01 = pd1.getText().toString().trim();
+                String pd02  = pd1.getText().toString().trim();
+                String pd03 = pd1.getText().toString().trim();
+                String pd04 = pd1.getText().toString().trim();
+
+                Bundle personal_detail = new Bundle();
+                personal_detail.putString("name", pd01);
+                personal_detail.putString("number", pd02);
+                personal_detail.putString("email", pd03);
+                personal_detail.putString("date", pd03
+                );
+
                 if (pd01.isEmpty()) {
-                    pd1.setError("Enter Name!");
-                }
-                else {
+                    pd1.setError("Please enter name!");
+                } else if (pd02.isEmpty()) {
+                    pd2.setError("Please enter number!");
+                } else if (pd03.isEmpty()) {
+                    pd3.setError("Please enter Address!");
+                } else if (pd04.isEmpty()) {
+                    pd4.setError("Please enter email!");
+                } else {
 
 
                     Intent Intent = new Intent(Personal_Information.this, Work_History.class);
